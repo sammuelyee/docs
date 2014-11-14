ded4880c5cfe128a404c277df5cde2892d0f63b2
Raul Ochoa <rochoaf@gmail.com>
2014-11-14 17:51:45 +0100

    Apply regex before generating documentation for APIs for highlighting.

--- docs_build.sh
+++ docs_build.sh
@@ -12,8 +12,19 @@ cp _cartodb-js.md _cartodb-platform/cartodb-js.md
 cp _sql-api.md _cartodb-platform/sql-api.md
 cp _maps-api.md _cartodb-platform/maps-api.md
 
+sed -i.bak -E 's/^```(.+)/{% highlight \1 %}/g' API-js.md
+sed -i.bak -E 's/^```/{% endhighlight %}/g' API-js.md
+sed -i.bak -E 's/\{% raw %\}//' API-js.md
+sed -i.bak -E 's/\{% endraw %\}//' API-js.md
+sed -i.bak -E 's/\{\{(.*)\}\}/{% raw %}{{\1}}{% endraw %}/' API-js.md
 cat API-js.md >> _cartodb-platform/cartodb-js.md
+
+sed -i.bak -E 's/^```(.+)/{% highlight \1 %}/g' API-sql.md
+sed -i.bak -E 's/^```/{% endhighlight %}/g' API-sql.md
 cat API-sql.md >> _cartodb-platform/sql-api.md
+
+sed -i.bak -E 's/^```(.+)/{% highlight \1 %}/g' Map-API.md
+sed -i.bak -E 's/^```/{% endhighlight %}/g' Map-API.md
 cat Map-API.md >> _cartodb-platform/maps-api.md
 
 rm API-js.md
